35dc8d1d1410ccf5ffb643ae4456f47f35a6847d
HRileyTang <hrileytang@gmail.com>
2023-03-19 18:46:51 -0700

    timers use strings for ids instead of ints

--- MonoEngine/Engine/Core/Time.cs
+++ MonoEngine/Engine/Core/Time.cs
@@ -1,77 +1,63 @@
 ﻿using System.Collections.Generic;
 
-namespace MonoEngine.Engine.Core
-{
-    public static class Time
-    {
+namespace FlappyBird.Engine.Core {
+    public static class Time {
         public static double now { get; private set; }
-        public static double deltaTime { get; private set; }
+        public static float deltaTime { get; private set; }
 
-        private static Dictionary<int, Timer> timers = new Dictionary<int, Timer>();
+        private static Dictionary<string, Timer> timers = new Dictionary<string, Timer>();
 
-        public static void Update(double timeSinceGameStart)
-        {
-            deltaTime = timeSinceGameStart - now;
+        public static void Update(double timeSinceGameStart) {
+            deltaTime = (float)(timeSinceGameStart - now);
             now = timeSinceGameStart;
             foreach (Timer t in timers.Values)
                 t.Update(deltaTime);
         }
 
-        //returns id of timer (should be stored by caller)
-        public static int StartNewTimer()
-        {
-            //find next available timer id
-            int i = 0;
-            while (timers.ContainsKey(i))
-                i++;
-            timers.Add(i, new Timer());
-            return i;
+        public static void StartNewTimer(string timerId) {
+            if (!timers.ContainsKey(timerId))
+                timers.Add(timerId, new Timer());
         }
 
-        public static double GetElapsedTimeOfTimer(int timerId)
-        {
+        public static double GetElapsedTimeOfTimer(string timerId) {
             return timers.ContainsKey(timerId) ? timers[timerId].timeElapsed : 0;
         }
 
-        public static double GetDeltaTimeOfTimer(int timerId)
-        {
+        public static float GetDeltaTimeOfTimer(string timerId) {
             return timers.ContainsKey(timerId) ? timers[timerId].deltaTime : 0;
         }
 
-        public static void ResumeTimer(int timerId)
-        {
+        public static void ResumeTimer(string timerId) {
             if (timers.ContainsKey(timerId)) timers[timerId].active = true;
         }
 
-        public static void PauseTimer(int timerId)
-        {
+        public static void PauseTimer(string timerId) {
             if (timers.ContainsKey(timerId)) timers[timerId].active = false;
         }
 
-        public static void DeleteTimer(int timerId)
-        {
+        public static void ResetTimer(string timerId) {
+            if (timers.ContainsKey(timerId)) timers[timerId].timeElapsed = 0;
+        }
+
+        public static void DeleteTimer(string timerId) {
             if (timers.ContainsKey(timerId))
                 timers.Remove(timerId);
         }
     }
 
-    public class Timer
-    {
+    public class Timer {
         public bool active;
-        public double deltaTime { get; private set; }
-        public double timeElapsed { get; private set; }
+        public float deltaTime;
+        public double timeElapsed;
 
-        public Timer()
-        {
+        public Timer() {
             active = true;
             deltaTime = 0;
             timeElapsed = 0;
         }
 
-        public void Update(double deltaTime)
-        {
-            if (active)
-            {
+        public void Update(float deltaTime) {
+            if (active) {
                 this.deltaTime = deltaTime;
                 timeElapsed += deltaTime;
             }
